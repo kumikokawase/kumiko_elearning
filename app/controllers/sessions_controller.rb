bf11478ccea3b66040451a953aea614ec21a7146
KumikoKawase <kumikokawase@kawasetakayukis-MacBook-Air.local>
2021-04-15 00:49:33 +0900

    making Signup/Login pages

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      flash[:success] = "Successfully logged in!"
+      log_in user
+      redirect_to root_url
+    else
+      render 'new'
+      flash.now[:danger] = "Invalid content. Try again."  
+    end
+  end
+
+  def destroy
+    log_out
+    
+    redirect_to root_url
+  end
+end
