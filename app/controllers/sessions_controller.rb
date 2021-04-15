b27e473e3f26db5aa4a0abd156a0122c800b9044
KumikoKawase <kumikokawase@kawasetakayukis-MacBook-Air.local>
2021-04-15 22:49:51 +0900

    Fixing spaces

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,24 +1,24 @@
 class SessionsController < ApplicationController
 
-    def new
-    end
+  def new
+  end
 
-    def create
-        user = User.find_by(email: params[:session][:email].downcase)
-        if user && user.authenticate(params[:session][:password])
-            flash[:success] = "Successfully logged in!"
-            log_in user
-            redirect_to root_url
-        else
-            render 'new'
-            flash.now[:danger] = "Invalid content. Try again."  
-        end
-    end
-
-    def destroy
-        log_out
-        
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+        flash[:success] = "Successfully logged in!"
+        log_in user
         redirect_to root_url
+    else
+        render 'new'
+        flash.now[:danger] = "Invalid content. Try again."  
     end
+  end
+
+  def destroy
+    log_out
+        
+    redirect_to root_url
+  end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,61 +1,61 @@
 class UsersController < ApplicationController
 
-    def index
-        @users = User.all
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Saved successfully!"   
+      redirect_to login_url
+    else
+      flash[:danger] = "Invalid content. Try again."
+      render 'new'
     end
+  end
 
-    def new
-        @user = User.new
-    end
+  def show
+    @user = User.find(params[:id])
+  end
 
-    def create
-        @user = User.new(user_params)
-        if @user.save
-            flash[:success] = "Saved successfully!"   
-            redirect_to login_url
-        else
-            flash[:danger] = "Invalid content. Try again."
-            render 'new'
-        end
-    end
+  def edit
+    @user = User.find(params[:id])
+  end 
 
-    def show
-        @user = User.find(params[:id])
-    end
-
-    def edit
-        @user = User.find(params[:id])
-    end 
-
-    def update
-        @user = User.find(params[:id])
-        if @user.update_atributes(user_params)
-            flash[:success] = "Saved successfully!"
-            redirect_to @user
-        else
-            flash[:danger] = "Invalid content. Try again."
-            render 'edit'  
-        end  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_atributes(user_params)
+      flash[:success] = "Saved successfully!"
+      redirect_to @user
+    else
+      flash[:danger] = "Invalid content. Try again."
+      render 'edit'  
     end  
+  end  
 
-    def destroy
-        User.find(params[:id]).destroy
-        redirect_to users_url
-    end 
+  def destroy
+    User.find(params[:id]).destroy
+    redirect_to users_url
+  end 
 
-    private
+  private
 
-    def user_params
-        params.require(:user).permit(:name, :email, :password, :password_confirmation)
-    end
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 
-    def correct_user
-        @user = User.find(params[:id])
-        # go to HOME PAGE
-        # UNLESS
-        # current user? -> ID of this user
-        # is the same as the ID of the user being checked
-        redirect_to(root_url) unless current_user?(@user)
-    end
+  def correct_user
+    @user = User.find(params[:id])
+    # go to HOME PAGE
+    # UNLESS
+    # current user? -> ID of this user
+    # is the same as the ID of the user being checked
+    redirect_to(root_url) unless current_user?(@user)
+  end
   
 end
