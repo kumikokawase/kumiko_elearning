53b1435b12ad420cf68d30be57152dea868f8f7b
kumikokawase <82156430+kumikokawase@users.noreply.github.com>
2021-04-16 19:39:57 +0900

    Signup (#2)
    * Making new Signup branch & fixing Signup pages
    
    * Fixing spaces
    
    Co-authored-by: KumikoKawase <kumikokawase@kawasetakayukis-MacBook-Air.local>

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,31 @@
+module SessionsHelper
+  # Logs in the given user.
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+    
+  # Returns true if the given user is the current user.
+  def current_user?(user)
+    user == current_user
+  end
+    
+  # Returns the current logged-in user (if any).
+  def current_user # after log_in -> current_user
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+  end
+
+  # Returns true if the user is logged in, false otherwise.
+  def logged_in?
+    !current_user.nil? # is there NO EMPTY current_user? Is there a current_user?
+    # is the current user empty? TRUE
+    # is the current user NOT empty? FALSE
+  end
+    
+  # Logs out the current user.
+  def log_out
+    session.delete(:user_id) # deletes memory in browser
+    @current_user = nil       # deletes you as current_user
+  end    
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,25 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   root 'static_pages#home'
   
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
+   
+  get '/signup', to: 'users#new' 
+
+   # /login
+   get '/login', to: 'sessions#new'
+
+   # /logout
+   delete '/logout', to: 'sessions#destroy'
+ 
+   resources :users
+     
+   # Sessions
+   resources :sessions, only: [:new, :create, :destroy]
+  
 
 end
