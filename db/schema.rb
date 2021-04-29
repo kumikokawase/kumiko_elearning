7a256f74f09721138b97daaecbe2cc0bee81ed87
KumikoKawase <kumikokawase@kawasetakayukis-MacBook-Air.local>
2021-04-29 22:15:40 +0900

    make answer_pages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_19_134812) do
+ActiveRecord::Schema.define(version: 2021_04_29_123332) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
